7c0090f7e6ff5c714bcc8e30e3b78ec4486691a2
Bob Sponge <spongebobsp22@gmail.com>
2024-07-24 13:52:40 +0300

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["registration/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"registration/test/unit/controller/ViewMain.controller"
+], function () {
+	"use strict";
+});
