a792786095aa57eee8bf798731b59695ecf97551
o.tararenko <o.tararenko@beloil.by>
2024-07-25 15:00:43 +0300

    all without save

--- webapp/controller/ViewMain.controller.js
+++ webapp/controller/ViewMain.controller.js
@@ -1,15 +1,43 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    'sap/ui/model/json/JSONModel'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, JSONModel) {
         "use strict";
 
         return Controller.extend("registration.controller.ViewMain", {
             onInit: function () {
 
+                var viewProperties = {
+                    bEnableUpdate: false
+                };
+                var viewModel = new sap.ui.model.json.JSONModel(viewProperties);
+                this.getView().setModel(viewModel, "viewModel");
+
+            
+            },
+
+            
+
+            onButtonPress: function() {
+
+
+            },
+
+
+            onParentClicked: function (oEvent) {
+                var bSelected = oEvent.getParameter("selected");
+                if (bSelected == true) {
+
+                    this.getView().getModel("viewModel").setProperty("/bEnableUpdate", true);
+                }
+                else { 
+                    this.getView().getModel("viewModel").setProperty("/bEnableUpdate", false); 
+                }
             }
+
         });
     });
