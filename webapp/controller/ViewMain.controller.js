822e0269632f6bd9e9a2eb56dd4ee74104840c0a
Bob Sponge <spongebobsp22@gmail.com>
2024-07-29 16:56:16 +0300

    before oksana's birthday

--- webapp/controller/ViewMain.controller.js
+++ webapp/controller/ViewMain.controller.js
@@ -1,14 +1,19 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    'sap/ui/model/json/JSONModel'
+    'sap/ui/model/json/JSONModel',
+    'sap/m/MessageToast'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
+
+
+
     function (Controller, JSONModel) {
         "use strict";
-
         return Controller.extend("registration.controller.ViewMain", {
+
+
             onInit: function () {
 
                 var viewProperties = {
@@ -16,12 +21,13 @@ sap.ui.define([
                 };
                 var viewModel = new sap.ui.model.json.JSONModel(viewProperties);
                 this.getView().setModel(viewModel, "viewModel");
+            },
 
+            _createEntity: function () {
 
             },
 
             onButtonPress: function () {
-                var ret = -1;
                 var nachn = this.getView().byId("nachn").getValue();
                 var login = this.getView().byId("login").getValue();
                 var password = this.getView().byId("password").getValue();
@@ -46,87 +52,55 @@ sap.ui.define([
                 }
 
                 var oModel = this.getView().getModel();
-                oModel.setTokenHandlingEnabled(true);
-                var oContext = oModel.createEntry("/ZHR_C_CANDIDATE_REGS", {
-                    properties: {
-                        vorna: this.getView().byId("vorna").getValue(),
-                        nachn: this.getView().byId("nachn").getValue(),
-                        nach2: this.getView().byId("nachn2").getValue(),
-                        pernr: this.getView().byId("tabel").getValue(),
-                        useralias: this.getView().byId("login").getValue(),
-                        password: this.getView().byId("password").getValue(),
-                        num01_email: this.getView().byId("mail").getValue(),
-
-                    }
-                });
-                oModel.submitChanges();
-
-                var Url = "/ZHR_C_CANDIDATE_REGS(useralias='"+login+"')?$expand=UserRoleCollection";
-
-
-                oModel.read( Url, {
-                    async: false,
+                var Url = "/ZHR_C_CANDIDATE_REGS";
+                var filters = new Array();
+                var filterByName = new sap.ui.model.Filter("IsActiveEntity", sap.ui.model.FilterOperator.EQ, "false")
+                filters.push(filterByName);
+                var filterByName = new sap.ui.model.Filter("num01_email", sap.ui.model.FilterOperator.EQ, mail)
+                filters.push(filterByName);
+                oModel.read(Url, {
+                    filters: filters,
                     success: function (oData, oResponse) {
-                        ret = oData;
+                        var ret = oData.results;
+                        console.log("return", ret);
+                        if (ret.length != 0) {
+                            alert("Данная почта зареригистрирована, пожалуйста введите другую")
+                            return;
+                        };
+                        var oModel = this.getView().getModel();
+                        oModel.setTokenHandlingEnabled(true);
+
+                        var oContext = oModel.createEntry("/ZHR_C_CANDIDATE_REGS", {
+                            properties: {
+                                vorna: this.getView().byId("vorna").getValue(),
+                                nachn: this.getView().byId("nachn").getValue(),
+                                nach2: this.getView().byId("nachn2").getValue(),
+                                pernr: this.getView().byId("tabel").getValue(),
+                                useralias: this.getView().byId("login").getValue(),
+                                password: this.getView().byId("password").getValue(),
+                                num01_email: this.getView().byId("mail").getValue(),
+
+                            }
+                        });
+                        oModel.submitChanges();
+                        oContext.created().then(
+                            function () {
+                                //window.open("https://sapbpc-dev.beloil.by/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#Shell-home", '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
+
+                            },
+                            function () { alert("Ошибка регистрации") }
+                        )
+
                     },
                     error: function (oError) {
-                        MessageToast.show(oError);
                         console.log(oError);
-                        ret = -1;
                         return;
                     }
-
-                });
-                console.log(ret);
-
-
-
-
-
-                //   oModel.read("/ZHR_C_CANDIDATE_REGS", {
-                //      function(oData, oResponse){
-                //          userAll = oData.useralias;
-                //   return JSON.stringify(oData);
-                //     },   }
-                //  );
-
-                //   console.log(userAll);
-
-                oModel.read("/ZHR_C_CANDIDATE_REGS", {
-                    success: function (oData, oResponse) {
-                        var oMessage = new sap.ui.core.message.Message({
-                            message: "We have received the following response: " + oResponse,
-                            persistent: true, // create message as transition message
-                            type: sap.ui.core.MessageType.Success
-
-
-                        });
-                        //"Messaging" required from module "sap/ui/core/Messaging";
-                        Messaging.addMessages(oMessage);
-                        fnResolve();
-                    },
-                    //  error: fnReject
-
-
-
                 });
 
 
 
 
-
-
-
-
-
-                oContext.created().then(
-                    function () {
-                        alert("Вы зарегистрированы. Данные отправлены на e-mail!")
-                        window.open("https://sapbpc-dev.beloil.by/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#Shell-home", '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
-
-                    },
-                    function () { alert("Ошибка регистрации") }
-                )
             },
 
 
