70451d96ad648e12d896e87cfe4cf0dafc5bd57e
Bob Sponge <spongebobsp22@gmail.com>
2024-07-30 15:06:46 +0300

    e-mail implemented oksana birthday 30072024

--- webapp/controller/ViewMain.controller.js
+++ webapp/controller/ViewMain.controller.js
@@ -1,20 +1,18 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    'sap/ui/model/json/JSONModel',
-    'sap/m/MessageToast'
+    'sap/m/MessageBox',
+    'sap/m/MessageToast',
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-
-
-
-    function (Controller, JSONModel) {
+    function (Controller,
+        MessageBox,
+        MessageToast) {
         "use strict";
+        let candidateEntity = "/ZHR_C_CANDIDATE_REGS";
         return Controller.extend("registration.controller.ViewMain", {
-
-
-            onInit: function () {
+            onInit: function()  {
 
                 var viewProperties = {
                     bEnableUpdate: false
@@ -23,8 +21,58 @@ sap.ui.define([
                 this.getView().setModel(viewModel, "viewModel");
             },
 
-            _createEntity: function () {
+            onSuccessRecordAdded: function()  {
+                oView.setBusy(false);
+                let oModel = this.getView().getModel();
+                let oView = this.getView();         
+                var filters = new Array();
+                filters.push(new sap.ui.model.Filter("IsActiveEntity", sap.ui.model.FilterOperator.EQ, "false"));
+                filters.push(new sap.ui.model.Filter("num01_email", sap.ui.model.FilterOperator.EQ, oView.byId("mail").getValue()));
+                filters.push(new sap.ui.model.Filter("zsap_user", sap.ui.model.FilterOperator.GT, ''));
+                oModel.read(candidateEntity, {
+                    filters: filters,
+                    success: (oData) => {
+                        if (oData.results.length != 0) {
+                            MessageToast.show('Анкета отправлена. \r\n Спасибо за регистрацию!');
+                            window.open("https://sapbpc-dev.beloil.by/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#Shell-home", '_blank');
+                        }
+                        else {
+                            MessageBox.error('Ошибка регистрации. \r\n Проверьте пожалуйста данные!');
+                        }
+                    },
+                    error: oError => MessageBox.error(JSON.parse(oError.responseText).error.message.value, { title: "Ошибка" })
+                });
 
+            },
+
+            onSuccessReadEmail: function (oData)  {
+                var oModel = this.getView().getModel();
+                var oView = this.getView();
+                var ret = oData.results;
+                console.log("return", ret);
+                if (ret.length != 0) {
+                    MessageBox.error("Анкета с такой почтой (" + oView.byId("mail").getValue() + ") уже зареригистрирована. \r\n Введите пожалуйста другую");
+                    return;
+                };
+                oView.setBusy(true);
+                var oContext = oModel.createEntry("/ZHR_C_CANDIDATE_REGS", {
+                    properties: {
+                        vorna: oView.byId("vorna").getValue(),
+                        nachn: oView.byId("nachn").getValue(),
+                        nach2: oView.byId("nachn2").getValue(),
+                        pernr: oView.byId("tabel").getValue(),
+                        useralias: oView.byId("login").getValue(),
+                        password: oView.byId("password").getValue(),
+                        num01_email: oView.byId("mail").getValue(),
+                    }
+                });
+                oModel.submitChanges({
+                    success: this.onSuccessRecordAdded.bind(this),
+                    error: oError => {
+                        MessageBox.error(JSON.parse(oError.responseText).error.message.value, { title: "Ошибка" });
+                        oView.setBusy(false)
+                    }
+                });
             },
 
             onButtonPress: function () {
@@ -37,72 +85,31 @@ sap.ui.define([
 
 
                 if (password != repeat_password) {
-                    alert("Пароли не совпадают")
+                    alert("Пароли не совпадают");
                     return;
                 }
 
                 if (mail != repeat_mail) {
-                    alert("Почта не совпадает")
+                    alert("Почта не совпадает");
                     return;
                 }
 
                 if (nachn == "" || login == "" || password == "" || repeat_password == "" || mail == "" || repeat_mail == "") {
-                    alert("Заполните обязательные поля")
+                    alert("Заполните обязательные поля");
                     return;
                 }
 
                 var oModel = this.getView().getModel();
-                var Url = "/ZHR_C_CANDIDATE_REGS";
+                oModel.setTokenHandlingEnabled(true);
                 var filters = new Array();
-                var filterByName = new sap.ui.model.Filter("IsActiveEntity", sap.ui.model.FilterOperator.EQ, "false")
-                filters.push(filterByName);
-                var filterByName = new sap.ui.model.Filter("num01_email", sap.ui.model.FilterOperator.EQ, mail)
-                filters.push(filterByName);
-                oModel.read(Url, {
+                filters.push(new sap.ui.model.Filter("IsActiveEntity", sap.ui.model.FilterOperator.EQ, "false"));
+                filters.push(new sap.ui.model.Filter("num01_email", sap.ui.model.FilterOperator.EQ, mail));
+                oModel.read(candidateEntity, {
                     filters: filters,
-                    success: function (oData, oResponse) {
-                        var ret = oData.results;
-                        console.log("return", ret);
-                        if (ret.length != 0) {
-                            alert("Данная почта зареригистрирована, пожалуйста введите другую")
-                            return;
-                        };
-                        var oModel = this.getView().getModel();
-                        oModel.setTokenHandlingEnabled(true);
-
-                        var oContext = oModel.createEntry("/ZHR_C_CANDIDATE_REGS", {
-                            properties: {
-                                vorna: this.getView().byId("vorna").getValue(),
-                                nachn: this.getView().byId("nachn").getValue(),
-                                nach2: this.getView().byId("nachn2").getValue(),
-                                pernr: this.getView().byId("tabel").getValue(),
-                                useralias: this.getView().byId("login").getValue(),
-                                password: this.getView().byId("password").getValue(),
-                                num01_email: this.getView().byId("mail").getValue(),
-
-                            }
-                        });
-                        oModel.submitChanges();
-                        oContext.created().then(
-                            function () {
-                                //window.open("https://sapbpc-dev.beloil.by/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#Shell-home", '_blank', 'location=yes,height=570,width=520,scrollbars=yes,status=yes');
-
-                            },
-                            function () { alert("Ошибка регистрации") }
-                        )
-
-                    },
-                    error: function (oError) {
-                        console.log(oError);
-                        return;
-                    }
+                    success: this.onSuccessReadEmail.bind(this),
+                    error: oError => MessageBox.error(JSON.parse(oError.responseText).error.message.value, { title: "Ошибка" })
                 });
-
-
-
-
             },
-
 
             onParentClicked: function (oEvent) {
                 var bSelected = oEvent.getParameter("selected");
@@ -114,6 +121,5 @@ sap.ui.define([
                     this.getView().getModel("viewModel").setProperty("/bEnableUpdate", false);
                 }
             }
-
         });
     });
