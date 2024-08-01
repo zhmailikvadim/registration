af6837135f086fd6977325cee072bf79500b65d3
Bob Sponge <spongebobsp22@gmail.com>
2024-08-01 16:17:32 +0300

    it works with filter has_errors

--- webapp/controller/ViewMain.controller.js
+++ webapp/controller/ViewMain.controller.js
@@ -19,30 +19,27 @@ sap.ui.define(
 
       onAnyError: function (oData) {
         if (oData.results.length > 0) {
-       /*   MessageToast.show('Анкета отправлена. \r\n Спасибо за регистрацию!');
-          var new_window = window.open('https://sapbpc-dev.beloil.by/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#Shell-home', '_blank');
-        } else {*/
-          
-          /////////////
-          let oModelLog = this.getView().getModel();
-          var filters1 = new Array();
-        //  filters1.push(new sap.ui.model.Filter('Active', sap.ui.model.FilterOperator.EQ, 'true'));
-          filters1.push(new sap.ui.model.Filter('IsActiveEntity', sap.ui.model.FilterOperator.EQ, 'false'));
-          filters1.push(new sap.ui.model.Filter('uuid', sap.ui.model.FilterOperator.EQ, oData.results[ 0 ].uuid));
-          oModelLog.read(candidateEntity, {
-            filters: filters1,
-            urlParameters: { "$expand": "to_RecruitmentLog" },
-            success: function(data, response) { 
-              console.log(response),
-              console.log(data),
-              MessageBox.error('Ошибка регистрации. \r\n Проверьте пожалуйста данные!'); 
-            },
-          });
-
-          ///////////
-        //  MessageBox.error('Ошибка регистрации. \r\n Проверьте пожалуйста данные!');
-       
-
+          if (oData.results[0].has_errors == true) {
+            let oModelLog = this.getView().getModel();
+            var filters1 = new Array();
+            filters1.push(new sap.ui.model.Filter('IsActiveEntity', sap.ui.model.FilterOperator.EQ, 'false'));
+            filters1.push(new sap.ui.model.Filter('uuid', sap.ui.model.FilterOperator.EQ, oData.results[0].uuid));
+            oModelLog.read(candidateEntity, {
+              filters: filters1,
+              urlParameters: { $expand: 'to_RecruitmentLog' },
+              success: function (data, response) {
+                console.log(response),
+                  console.log(data),
+                  MessageBox.error(data.results[0].to_RecruitmentLog.results[0].message + data.results[0].to_RecruitmentLog.results[0].message_v4);
+              },
+            });
+          } else {
+            MessageToast.show('Анкета отправлена. \r\n Спасибо за регистрацию!');
+            var new_window = window.open(
+              'https://sapbpc-dev.beloil.by/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#Shell-home',
+              '_blank',
+            );
+          }
         }
       },
 
@@ -53,7 +50,7 @@ sap.ui.define(
         var filters = new Array();
         filters.push(new sap.ui.model.Filter('IsActiveEntity', sap.ui.model.FilterOperator.EQ, 'false'));
         filters.push(new sap.ui.model.Filter('num01_email', sap.ui.model.FilterOperator.EQ, oView.byId('mail').getValue()));
-        //filters.push(new sap.ui.model.Filter('zsap_user', sap.ui.model.FilterOperator.GT, ''));
+
         oModel.read(candidateEntity, {
           filters: filters,
           success: this.onAnyError.bind(this),
@@ -62,7 +59,10 @@ sap.ui.define(
       },
 
       onSapLogonPress: function () {
-        var new_window = window.open('https://sapbpc-dev.beloil.by/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#EnvironmentalData-explore', '_blank');
+        var new_window = window.open(
+          'https://sapbpc-dev.beloil.by/sap/bc/ui5_ui5/ui2/ushell/shells/abap/FioriLaunchpad.html#EnvironmentalData-explore',
+          '_blank',
+        );
         new_window.onload = function () {
           new_window.document.getElementById('USERNAME_FIELD-inner').value = '111111';
           const inputElement = new_window.document.querySelector('#USERNAME_FIELD-inner');
@@ -125,7 +125,10 @@ sap.ui.define(
         oModel.setTokenHandlingEnabled(true);
         var filters = new Array();
         filters.push(new sap.ui.model.Filter('IsActiveEntity', sap.ui.model.FilterOperator.EQ, 'false'));
+        filters.push(new sap.ui.model.Filter('has_errors', sap.ui.model.FilterOperator.EQ, 'false'));
         filters.push(new sap.ui.model.Filter('num01_email', sap.ui.model.FilterOperator.EQ, mail));
+
+        //Read for checking e-mail
         oModel.read(candidateEntity, {
           filters: filters,
           success: this.onSuccessReadEmail.bind(this),
